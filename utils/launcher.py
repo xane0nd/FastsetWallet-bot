5ec8b641a549932ab9f80daa55e1fcd6920efba9
Xan <30492020+xane0nd@users.noreply.github.com>
2025-10-23 19:29:19 +0200

    v 1.0.3

--- utils/launcher.py
+++ utils/launcher.py
@@ -5,7 +5,7 @@ from typing import Any
 from better_proxy import Proxy
 
 from bot.config import settings
-from bot.utils import logger
+from bot.utils import logger 
 from bot.core.tapper import run_tapper
 from bot.core.registrator import register_sessions
 from bot.utils.accounts import Accounts
